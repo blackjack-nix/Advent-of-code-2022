defdd1f23eb4cab2b2fc57bdeb870a71d5a30e0e
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2022-12-01 19:34:50 +0100

    flex on 5 liner

--- day1.py
+++ day1.py
@@ -1,7 +1,5 @@
 count, lst, l = 0, [], open("adventofcode1.txt","r")
 for l in l.readlines():
-    if l.strip() == "":
-        lst.append(count)
-        count = 0
+    if l.strip() == "":lst.append(count);count = 0
     else : count += int(l.strip())
 print(f"Part 1 (biggest) : {sorted(lst)[-1]}\nPart 2 (sum of top 3 biggest) : {sum(sorted(lst, reverse=True)[:3])}")
