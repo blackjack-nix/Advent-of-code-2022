20871b086c9d3b1d81a7b310ff8a415114c1c148
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2022-12-01 19:04:10 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,8 +1,7 @@
-count, lst = 0, []
-with open("adventofcode1.txt","r") as f:
-    for l in f.readlines():
-        if l.strip() == "":
-            lst.append(count)
-            count = 0
-        else : count += int(l.strip())
-    print(f"Part 1 (biggest) : {sorted(lst)[-1]}\nPart 2 (sum of top 3 biggest) : {sum(sorted(lst, reverse=True)[:3])}")
+count, lst, l = 0, [], open("adventofcode1.txt","r")
+for l in l.readlines():
+    if l.strip() == "":
+        lst.append(count)
+        count = 0
+    else : count += int(l.strip())
+print(f"Part 1 (biggest) : {sorted(lst)[-1]}\nPart 2 (sum of top 3 biggest) : {sum(sorted(lst, reverse=True)[:3])}")
