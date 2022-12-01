cb116a51c331e81511f3103d3795f92a17bad224
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2022-12-01 18:58:09 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,14 +1,9 @@
-biggest = 0
 count = 0
 lst = []
 with open("adventofcode1.txt","r") as f:
     for l in f.readlines():
         if l.strip() == "":
-            if count > biggest:
-                biggest = count
             lst.append(count)
             count = 0
-        else : 
-            count += int(l.strip())
-    print(f"Part 1 (biggest) : {biggest}")
-    print(f"Part 2 (sum of top 3 biggest) : {sorted(lst)[-1] + sorted(lst)[-2] + sorted(lst)[-3]}")
+        else : count += int(l.strip())
+    print(f"Part 1 (biggest) : {sorted(lst)[-1]}\nPart 2 (sum of top 3 biggest) : {sum(sorted(lst, reverse=True)[:3])}")
